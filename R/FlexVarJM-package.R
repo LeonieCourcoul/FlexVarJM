fe7b7220a1459aecf63d04786e5aead4f55b6e69
COURCOUL Léonie <lc14@dim-ub2.local>
2023-08-08 15:28:47 +0200

    FinMaj

--- R/FlexVarJM-package.R
+++ R/FlexVarJM-package.R
@@ -1,6 +1,11 @@
+#' @useDynLib FlexVarJM, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+NULL
+
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
